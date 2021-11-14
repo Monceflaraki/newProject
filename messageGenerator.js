881e27be5f17d8cc263f9808aa2d6ba2771bc91d
moncef laraki <moncef.fissellaraki@gmail.com>
2021-11-14 18:57:47 +0100

    add first 3 message to the object

---
+++ messageGenerator.js
@@ -0,0 +1,5 @@
+let myMessages = {
+    msg1:"It always seems impossible until it's done.",
+    msg2:"Setting goals is the first step in turning the invisible into the visible.",
+    msg3:"Problems are not stop signs, they are guidelines.",
+}
