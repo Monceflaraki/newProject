bcf4c207aa47fc26ae25527c3146d7d3f5a63888
moncef laraki <moncef.fissellaraki@gmail.com>
2021-11-14 19:01:33 +0100

    add 3 more msg

--- messageGenerator.js
+++ messageGenerator.js
@@ -2,4 +2,8 @@ let myMessages = {
     msg1:"It always seems impossible until it's done.",
     msg2:"Setting goals is the first step in turning the invisible into the visible.",
     msg3:"Problems are not stop signs, they are guidelines.",
+    msg4:"There is no great genius without some touch of madness.",
+    msg5:"Quality is not an act, it is a habit.",
+    msg6:"You simply have to put one foot in front of the other and keep going. Put blinders on and plow right ahead.",
+    
 }
