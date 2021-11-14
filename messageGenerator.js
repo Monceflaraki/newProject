9503a458edfa819ad1e32b6dbf6d7af6917fbd6b
moncef laraki <moncef.fissellaraki@gmail.com>
2021-11-14 19:05:36 +0100

    Have 9 message at total in the object

--- messageGenerator.js
+++ messageGenerator.js
@@ -5,5 +5,8 @@ let myMessages = {
     msg4:"There is no great genius without some touch of madness.",
     msg5:"Quality is not an act, it is a habit.",
     msg6:"You simply have to put one foot in front of the other and keep going. Put blinders on and plow right ahead.",
-    
+    msg7:"If you want to conquer fear, don't sit home and think about it. Go out and get busy.",
+    msg8:"In order to succeed, we must first believe that we can.",
+    msg9:"Even if you fall on your face, you're still moving forward.",
 }
+
